a221349c5c3d1e3b59d0063cbb38eb88787ff30c
Leo Chou <lchou@dec2176.hsn.de.hpc.ucar.edu>
2026-06-11 10:05:21 -0600

    Basic OpenMP, Defined nclumps as 1

--- offline_driver/CLMml.F90
+++ offline_driver/CLMml.F90
@@ -2,10 +2,13 @@ program CLMml
 
   use decompMod, only : bounds_type, get_clump_bounds
   use CLMml_driver, only : CLMml_drv
-
-  integer :: nc
+  implicit none
+  integer :: nc, nclumps
+  
 
   type(bounds_type) :: bounds
+
+  nclumps=1
 
   ! Define grid cell (g), land unit (l), column (c), and patch (p) bounds
   ! for CLM g/l/c/p hierarchy. CLM processes clumps of gridcells and
@@ -14,11 +17,13 @@ program CLMml
   ! that a grid cell has one land unit with one column and one patch. It
   ! processes a single grid cell.
 
-  nc = 1
-  call get_clump_bounds (nc, bounds)
-
-  ! Run model
-
-  call CLMml_drv (bounds)
+  
+  !call decompInit(ntower)
+  !$OMP PARALLEL DO PRIVATE(bounds, nc) SCHEDULE(DYNAMIC)
+  do nc = 1, nclumps
+      call get_clump_bounds (nc, bounds)
+      call CLMml_drv (bounds)
+  end do
+  !$OMP END PARALLEL DO
 
 end program CLMml
