bbf659aeb8a71d2da9f11a680f6192891cb3b384
Leo Chou <lchou@derecho4.hsn.de.hpc.ucar.edu>
2026-06-17 12:18:28 -0600

    Added logfile specification

--- offline_executable/run_OpenMP.sh
+++ offline_executable/run_OpenMP.sh
@@ -18,5 +18,9 @@ module restore CLM-OpenMPI
 export LIB_NETCDF=$NCAR_LDFLAGS_NETCDF; export MOD_NETCDF=$NCAR_INC_NETCDF
 cd /glade/u/home/lchou/CLM-ml_Sam/offline_executable
 
+HASH=$(git rev-parse --short HEAD 2>/dev/null || echo "nohash")
+DATETIME=$(date +"%Y%m%d_%H%M%S")
+LOGFILE="run.${HASH}.${DATETIME}_OpenMP.log"
+
 export OMP_NUM_THREADS=3
-./prgm.exe < nl.all_CHATS7.05.2007 > run.log 2>&1
+./prgm.exe < nl.all_CHATS7.05.2007 > "$LOGFILE" 2>&1
