d7035481cf85a02e7b45303ee190f62b773c45d8
Leo Chou <lchou@derecho6.hsn.de.hpc.ucar.edu>
2026-06-22 01:40:33 -0600

    Separate directory to ensure good dependency

---
+++ offline_executable_serial/run_serial.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#PBS -N CLMml_CHATS7_log_serial
+#PBS -A CESM0029
+#PBS -q main
+#PBS -l select=1:ncpus=1:mem=32GB
+#PBS -l walltime=00:30:00
+#PBS -j oe
+#PBS -l job_priority=regular
+
+module purge --force
+module load ncarenv/24.12
+module load conda/latest
+module load nvhpc/25.9
+module load ncarcompilers/1.0.0
+module load cuda/12.3.2
+module load libfabric/2.1.0
+module load openmpi/5.0.7
+module load hdf5-mpi/1.12.3
+module load netcdf-mpi/4.9.3
+module load parallel-netcdf/1.14.0
+export LIB_NETCDF=$NCAR_LDFLAGS_NETCDF; export MOD_NETCDF=$NCAR_INC_NETCDF
+cd /glade/u/home/lchou/CLM-ml_Sam/offline_executable_serial
+
+HASH=$(git rev-parse --short HEAD 2>/dev/null || echo "nohash")
+DATETIME=$(date +"%Y%m%d_%H%M%S")
+LOGFILE="run.${HASH}.${DATETIME}_serial.log"
+
+export OMP_NUM_THREADS=1
+./prgm.exe < nl.all_CHATS7.05.2007 > "$LOGFILE" 2>&1
