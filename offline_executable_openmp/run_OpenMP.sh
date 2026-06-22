a594b62a46c6616b121bb7ef14a8028a2cacb215
Leo Chou <lchou@derecho7.hsn.de.hpc.ucar.edu>
2026-06-22 13:15:22 -0600

    Changed back to intel... oops

--- offline_executable_openmp/run_OpenMP.sh
+++ offline_executable_openmp/run_OpenMP.sh
@@ -10,7 +10,8 @@
 module --force purge
 module load ncarenv/24.12
 module load conda/latest
-module load gcc/13.2.0
+#module load gcc/13.2.0
+module load intel/2024.2.1
 module load ncarcompilers/1.0.0
 module load cuda/12.3.2
 module load libfabric/2.1.0
