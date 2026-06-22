119400a0825f1e0f36fceed6046862f63dc4255e
Leo Chou <lchou@derecho1.hsn.de.hpc.ucar.edu>
2026-06-22 11:03:46 -0600

    Quick typo fix (accidentally wrote openMP rather than serial)

--- offline_executable/run_serial.sh
+++ offline_executable/run_serial.sh
@@ -36,7 +36,7 @@ echo "Cleaning previous builds..."
 make clean
 echo "Building the executable..."
 make 
-echo "Running the executable with OpenMP..."
+echo "Running the executable with serial..."
 
 export OMP_NUM_THREADS=1
 ./prgm.exe < nl.all_CHATS7.05.2007 > "$LOGFILE" 2>&1
