435c3a5391a4014edd317087cb5e29719f783c61
Leo Chou <lchou@derecho6.hsn.de.hpc.ucar.edu>
2026-06-22 02:23:28 -0600

    Added more buffer...? Hopefully it works?? No seg fault pleaseeeee

--- offline_executable_openmp/run_OpenMP.sh
+++ offline_executable_openmp/run_OpenMP.sh
@@ -25,6 +25,6 @@ HASH=$(git rev-parse --short HEAD 2>/dev/null || echo "nohash")
 DATETIME=$(date +"%Y%m%d_%H%M%S")
 LOGFILE="run.${HASH}.${DATETIME}_OpenMP.log"
 
-export OMP_STACKSIZE=256M
+export OMP_STACKSIZE=1028M
 export OMP_NUM_THREADS=3
 ./prgm.exe < nl.all_CHATS7.05.2007 > "$LOGFILE" 2>&1
