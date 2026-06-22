6a37c5ccdbba1306dc6e68df631d1cf57c9a9314
Leo Chou <lchou@derecho7.hsn.de.hpc.ucar.edu>
2026-06-22 10:36:38 -0600

    Readded make and make clean to shell script

--- offline_executable/run_serial.sh
+++ offline_executable/run_serial.sh
@@ -32,5 +32,11 @@ HASH=$(git rev-parse --short HEAD 2>/dev/null || echo "nohash")
 DATETIME=$(date +"%Y%m%d_%H%M%S")
 LOGFILE="run.${HASH}.${DATETIME}_serial.log"
 
+echo "Cleaning previous builds..."
+make clean
+echo "Building the executable..."
+make 
+echo "Running the executable with OpenMP..."
+
 export OMP_NUM_THREADS=1
 ./prgm.exe < nl.all_CHATS7.05.2007 > "$LOGFILE" 2>&1
--- offline_executable_openmp/run_OpenMP.sh
+++ offline_executable_openmp/run_OpenMP.sh
@@ -25,6 +25,12 @@ HASH=$(git rev-parse --short HEAD 2>/dev/null || echo "nohash")
 DATETIME=$(date +"%Y%m%d_%H%M%S")
 LOGFILE="run.${HASH}.${DATETIME}_OpenMP.log"
 
+echo "Cleaning previous builds..."
+make clean
+echo "Building the executable..."
+make 
+echo "Running the executable with OpenMP..."
+
 export OMP_STACKSIZE=1028M
 export OMP_NUM_THREADS=3
 ./prgm.exe < nl.all_CHATS7.05.2007 > "$LOGFILE" 2>&1
