4078a2ff7ba9202b215017821966d3a76701f678
Leo Chou <lchou@derecho7.hsn.de.hpc.ucar.edu>
2026-06-23 10:12:03 -0600

    Deleted all module that I loaded by myself, only the ones for CESM survives

--- offline_executable_openmp/run_OpenMP.sh
+++ offline_executable_openmp/run_OpenMP.sh
@@ -17,13 +17,13 @@ module load cmake
 #module load gcc/13.2.0
 module load intel/2025.3.2
 module load mkl
-module load ncarcompilers/1.0.0
-module load cuda/12.3.2
-module load libfabric/2.1.0
-module load openmpi/5.0.7
-module load hdf5-mpi/1.12.3
-module load netcdf-mpi/4.9.3
-module load parallel-netcdf/1.14.0
+#module load ncarcompilers/1.0.0
+#module load cuda/12.3.2
+#module load libfabric/2.1.0
+#module load openmpi/5.0.7
+#module load hdf5-mpi/1.12.3
+#module load netcdf-mpi/4.9.3
+#module load parallel-netcdf/1.14.0
 
 #
 export LIB_NETCDF=$NCAR_LDFLAGS_NETCDF; export MOD_NETCDF=$NCAR_INC_NETCDF
