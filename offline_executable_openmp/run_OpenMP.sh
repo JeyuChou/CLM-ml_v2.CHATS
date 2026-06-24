2a9db2ba2e196fca4adebc49264bbf32345d281f
Leo Chou <lchou@derecho4.hsn.de.hpc.ucar.edu>
2026-06-24 00:54:22 -0600

    Maybe something to do with bashrc... after modifying it, getting back all the module, trying to do cpudev to save time/resource

--- offline_executable_openmp/run_OpenMP.sh
+++ offline_executable_openmp/run_OpenMP.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #PBS -N CLMml_CHATS7_log_openMP
 #PBS -A CESM0029
-#PBS -q cpudev
+#PBS -q development
 #PBS -l select=1:ncpus=3:mem=32GB
-#PBS -l walltime=02:30:00
+#PBS -l walltime=00:30:00
 #PBS -j oe
 #PBS -l job_priority=regular
 
@@ -19,11 +19,11 @@ module load cmake
 #module load gcc/13.2.0
 module load intel/2025.3.2
 module load mkl
-#module load ncarcompilers/1.0.0
+module load ncarcompilers/1.0.0
 #module load cuda/12.3.2
-#module load libfabric/2.1.0
+module load libfabric/2.1.0
 module load openmpi/5.0.7
-#module load hdf5-mpi/1.12.3
+module load hdf5-mpi/1.12.3
 module load netcdf-mpi/4.9.3
 module load parallel-netcdf/1.14.0
 
