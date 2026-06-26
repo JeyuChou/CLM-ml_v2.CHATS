e2f558bc01c8463540722fe3647f3acf15aa7d87
Leo Chou <lchou@derecho5.hsn.de.hpc.ucar.edu>
2026-06-26 12:10:12 -0600

    Changed to only one thread to isolate issue

--- offline_executable_openmp/run_OpenMP.sh
+++ offline_executable_openmp/run_OpenMP.sh
@@ -2,7 +2,7 @@
 #PBS -N CLMml_CHATS7_log_openMP_OneThread
 #PBS -A CESM0029
 #PBS -q develop
-#PBS -l select=1:ncpus=3:mem=32GB
+#PBS -l select=1:ncpus=1:mem=32GB
 #PBS -l walltime=00:30:00
 #PBS -j oe
 #PBS -l job_priority=regular
