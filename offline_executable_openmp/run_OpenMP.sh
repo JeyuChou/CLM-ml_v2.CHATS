1bc13a8bcada8572bba883623df479f04a65dc1b
Sam Rabin <samrabin@ucar.edu>
2026-07-07 11:43:05 -0600

    run_OpenMP.sh: Genericize SCRIPT_DIR.

--- offline_executable_openmp/run_OpenMP.sh
+++ offline_executable_openmp/run_OpenMP.sh
@@ -29,7 +29,9 @@ module load parallel-netcdf/1.14.0
 
 #
 export LIB_NETCDF=$NCAR_LDFLAGS_NETCDF; export MOD_NETCDF=$NCAR_INC_NETCDF
-cd /glade/u/home/lchou/CLM-ml_Sam/offline_executable_openmp
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+cd "$SCRIPT_DIR"
+mkdir -p obj
 
 HASH=$(git rev-parse --short HEAD 2>/dev/null || echo "nohash")
 DATETIME=$(date +"%Y%m%d_%H%M%S")
