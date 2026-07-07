fce4f32907cd15a23fb7ef2164d2fd3f686909a6
Sam Rabin <samrabin@ucar.edu>
2026-07-07 14:32:19 -0600

    Add run_OpenMP_3threads.sh (premium)

---
+++ offline_executable_openmp/run_OpenMP_3threads.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#PBS -N CLMml_CHATS7_log_openMP_3Thread
+#PBS -A CESM0029
+#PBS -q main
+#PBS -l select=1:ncpus=3:mem=96GB
+#PBS -l walltime=00:30:00
+#PBS -j oe
+#PBS -l job_priority=premium
+
+set -euo pipefail
+
+export OMP_STACKSIZE=8G
+export OMP_NUM_THREADS=3
+
+. ./shared_openmp_setup.sh
